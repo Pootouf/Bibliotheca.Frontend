78fcf74b360a96559984f1ff72aa3155f6cb58b4
Etienne Binginot <etienne.binginot@gmail.com>
2024-08-31 22:55:25 +0200

    feat: add home page

--- Bibliotheca.Frontend/src/app/app.component.ts
+++ Bibliotheca.Frontend/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, NavComponent, HomeComponent],
+  standalone: true
 })
 export class AppComponent {
   
