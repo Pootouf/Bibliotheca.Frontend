7ab681d7800cee4753a65296d5d726d06a1f2590
Etienne Binginot <etienne.binginot@gmail.com>
2024-08-31 14:48:50 +0200

    feat: add model for animal observations

---
+++ Models/ImageData.cs
@@ -0,0 +1,11 @@
+﻿namespace Bibliotheca.Models
+{
+    public class ImageData
+    {
+        public Guid Id { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
---
+++ Models/Observation.cs
@@ -0,0 +1,17 @@
+﻿namespace Bibliotheca.Models
+{
+    public class Observation
+    {
+        public Guid Id { get; set; }
+
+        public ImageData? CoverImage { get; set; }
+
+        public IEnumerable<ImageData> Images { get; set; } = [];
+
+        public DateTime? ObservationDate { get; set; }
+
+        public DateTime? PostedDate { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
