0541fa84022644476bcaa104e27209f97d562963
Etienne Binginot <etienne.binginot@gmail.com>
2024-09-02 20:59:43 +0200

    feat: add getter for fields id

--- Bibliotheca.Frontend/src/app/search-bar/search-bar.component.ts
+++ Bibliotheca.Frontend/src/app/search-bar/search-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TaxonData } from '../shared/taxon-data';
 import { SpeciesService } from '../species.service';
 import { AutocompleteInputComponent } from '../autocomplete-input/autocomplete-input.component';
+import { TaxonRank } from '../shared/taxon-rank';
 
 @Component({
   selector: 'app-search-bar',
@@ -22,19 +23,28 @@ export class SearchBarComponent implements OnInit {
   speciesList: TaxonData[] = [];
   vernacularNameList: TaxonData[] = [];
 
+  kingdomRank: TaxonRank = new TaxonRank(-1, "KD");
+  phylumRank: TaxonRank = new TaxonRank(-1, "PH");
+  classRank: TaxonRank = new TaxonRank(-1, "CL");
+  orderRank: TaxonRank = new TaxonRank(-1, "OR");
+  familyRank: TaxonRank = new TaxonRank(-1, "FM");
+  tribeRank: TaxonRank = new TaxonRank(-1, "TR");
+  genusRank: TaxonRank = new TaxonRank(-1, "GN");
+  speciesRank: TaxonRank = new TaxonRank(-1, "ES");
+
   constructor(private speciesService: SpeciesService) {
 
   }
 
   async ngOnInit() {
-    this.kingdomList = await this.speciesService.getKingdomsData("");
-    this.phylumList = await this.speciesService.getPhylumsData("");
-    this.classList = await this.speciesService.getClassData("");
-    this.orderList = await this.speciesService.getOrderData("");
-    this.familyList = await this.speciesService.getFamilyData("");
-    this.tribeList = await this.speciesService.getTribeData("");
-    this.genusList = await this.speciesService.getGenusData("");
-    this.speciesList = await this.speciesService.getSpeciesData("");
+    this.kingdomList = await this.speciesService.getKingdomsData("", []);
+    this.phylumList = await this.speciesService.getPhylumsData("", []);
+    this.classList = await this.speciesService.getClassData("", []);
+    this.orderList = await this.speciesService.getOrderData("", []);
+    this.familyList = await this.speciesService.getFamilyData("", []);
+    this.tribeList = await this.speciesService.getTribeData("", []);
+    this.genusList = await this.speciesService.getGenusData("", []);
+    this.speciesList = await this.speciesService.getSpeciesData("", []);
   }
 
 
@@ -44,35 +54,43 @@ export class SearchBarComponent implements OnInit {
   }
 
   async onNewKingdomValue(newValue: string) {
-    this.kingdomList = await this.speciesService.getKingdomsData(newValue);
+    this.kingdomRank = await this.speciesService.getTaxonRankByValue(newValue, this.kingdomRank.rank);
+    this.kingdomList = await this.speciesService.getKingdomsData(newValue, [this.kingdomRank, this.phylumRank, this.classRank, this.orderRank, this.familyRank, this.tribeRank, this.genusRank, this.speciesRank]);
   }
 
   async onNewPhylumValue(newValue: string) {
-    this.phylumList = await this.speciesService.getPhylumsData(newValue);
+    this.phylumRank = await this.speciesService.getTaxonRankByValue(newValue, this.phylumRank.rank);
+    this.phylumList = await this.speciesService.getPhylumsData(newValue, [this.kingdomRank, this.phylumRank, this.classRank, this.orderRank, this.familyRank, this.tribeRank, this.genusRank, this.speciesRank]);
   }
 
   async onNewClassValue(newValue: string) {
-    this.classList = await this.speciesService.getClassData(newValue);
+    this.classRank = await this.speciesService.getTaxonRankByValue(newValue, this.classRank.rank);
+    this.classList = await this.speciesService.getClassData(newValue, [this.kingdomRank, this.phylumRank, this.classRank, this.orderRank, this.familyRank, this.tribeRank, this.genusRank, this.speciesRank]);
   }
 
   async onNewOrderValue(newValue: string) {
-    this.orderList = await this.speciesService.getOrderData(newValue);
+    this.orderRank = await this.speciesService.getTaxonRankByValue(newValue, this.orderRank.rank);
+    this.orderList = await this.speciesService.getOrderData(newValue, [this.kingdomRank, this.phylumRank, this.classRank, this.orderRank, this.familyRank, this.tribeRank, this.genusRank, this.speciesRank]);
   }
 
   async onNewFamilyValue(newValue: string) {
-    this.familyList = await this.speciesService.getFamilyData(newValue);
+    this.familyRank = await this.speciesService.getTaxonRankByValue(newValue, this.familyRank.rank);
+    this.familyList = await this.speciesService.getFamilyData(newValue, [this.kingdomRank, this.phylumRank, this.classRank, this.orderRank, this.familyRank, this.tribeRank, this.genusRank, this.speciesRank]);
   }
 
   async onNewTribeValue(newValue: string) {
-    this.tribeList = await this.speciesService.getTribeData(newValue);
+    this.tribeRank = await this.speciesService.getTaxonRankByValue(newValue, this.tribeRank.rank);
+    this.tribeList = await this.speciesService.getTribeData(newValue, [this.kingdomRank, this.phylumRank, this.classRank, this.orderRank, this.familyRank, this.tribeRank, this.genusRank, this.speciesRank]);
   }
 
   async onNewGenusValue(newValue: string) {
-    this.genusList = await this.speciesService.getGenusData(newValue);
+    this.genusRank = await this.speciesService.getTaxonRankByValue(newValue, this.genusRank.rank);
+    this.genusList = await this.speciesService.getGenusData(newValue, [this.kingdomRank, this.phylumRank, this.classRank, this.orderRank, this.familyRank, this.tribeRank, this.genusRank, this.speciesRank]);
   }
 
   async onNewSpeciesValue(newValue: string) {
-    this.speciesList = await this.speciesService.getSpeciesData(newValue);
+    this.speciesRank = await this.speciesService.getTaxonRankByValue(newValue, this.speciesRank.rank);
+    this.speciesList = await this.speciesService.getSpeciesData(newValue, [this.kingdomRank, this.phylumRank, this.classRank, this.orderRank, this.familyRank, this.tribeRank, this.genusRank, this.speciesRank]);
   }
 
 }
--- Bibliotheca.Frontend/src/app/species.service.ts
+++ Bibliotheca.Frontend/src/app/species.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { TaxonData } from './shared/taxon-data';
 import { environment } from '../environments/environment.development';
+import { TaxonRank } from './shared/taxon-rank';
 
 @Injectable({
   providedIn: 'root'
@@ -11,35 +12,48 @@ export class SpeciesService {
 
   constructor() { }
 
-  public async getKingdomsData(termBeginning: string) : Promise<TaxonData[]> {
+  public async getTaxonRankByValue(value: string, rank: string): Promise<TaxonRank> {
+    return await fetch(this.apiUrl + "/taxa/search?scientificNames==" + value +  "&taxonomicRanks=" + rank + "&page=1&size=1")
+            .then(res => res.ok ? res.json() : {page: {size: 0}})
+            .then(res => (
+              res.page.size == 0 ? 
+                new TaxonRank(-1, rank) : 
+                (res._embedded.taxa[0].scientificName == value ? 
+                    new TaxonRank(res._embedded.taxa[0].id, rank) : 
+                    new TaxonRank(-1, rank)
+                )
+            ));
+  }
+
+  public async getKingdomsData(termBeginning: string, taxonRanks: TaxonRank[]) : Promise<TaxonData[]> {
     return this.getData(termBeginning, "KD");
   }
 
-  public async getPhylumsData(termBeginning: string) : Promise<TaxonData[]> {
+  public async getPhylumsData(termBeginning: string, taxonRanks: TaxonRank[]) : Promise<TaxonData[]> {
     return this.getData(termBeginning, "PH");
   }
 
-  public async getClassData(termBeginning: string) : Promise<TaxonData[]> {
+  public async getClassData(termBeginning: string, taxonRanks: TaxonRank[]) : Promise<TaxonData[]> {
     return this.getData(termBeginning, "CL");
   }
 
-  public async getOrderData(termBeginning: string) : Promise<TaxonData[]> {
+  public async getOrderData(termBeginning: string, taxonRanks: TaxonRank[]) : Promise<TaxonData[]> {
     return this.getData(termBeginning, "OR");
   }
 
-  public async getFamilyData(termBeginning: string) : Promise<TaxonData[]> {
+  public async getFamilyData(termBeginning: string, taxonRanks: TaxonRank[]) : Promise<TaxonData[]> {
     return this.getData(termBeginning, "FM");
   }
 
-  public async getTribeData(termBeginning: string) : Promise<TaxonData[]> {
+  public async getTribeData(termBeginning: string, taxonRanks: TaxonRank[]) : Promise<TaxonData[]> {
     return this.getData(termBeginning, "TR");
   }
 
-  public async getGenusData(termBeginning: string) : Promise<TaxonData[]> {
+  public async getGenusData(termBeginning: string, taxonRanks: TaxonRank[]) : Promise<TaxonData[]> {
     return this.getData(termBeginning, "GN");
   }
 
-  public async getSpeciesData(termBeginning: string) : Promise<TaxonData[]> {
+  public async getSpeciesData(termBeginning: string, taxonRanks: TaxonRank[]) : Promise<TaxonData[]> {
     return this.getData(termBeginning, "ES");
   }
 
