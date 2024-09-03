d1df6c9d4e8f22c305cdabc768b18d5c9cddc359
Etienne Binginot <etienne.binginot@gmail.com>
2024-09-03 22:17:36 +0200

    feat: first implementation of synchronized filters

--- Bibliotheca.Frontend/src/app/species.service.ts
+++ Bibliotheca.Frontend/src/app/species.service.ts
@@ -13,7 +13,7 @@ export class SpeciesService {
   constructor() { }
 
   public async calculateNewTaxonRankIdByValue(value: string, rank: TaxonRank): Promise<TaxonRank> {
-    return await fetch(this.apiUrl + "/taxa/search?scientificNames==" + value +  "&taxonomicRanks=" + rank + "&page=1&size=1")
+    return await fetch(this.apiUrl + "/taxa/search?scientificNames=" + value +  "&taxonomicRanks=" + rank.rank + "&page=1&size=1")
             .then(res => res.ok ? res.json() : {page: {size: 0}})
             .then(res => {
               if (res.page.size == 0) {
@@ -30,13 +30,52 @@ export class SpeciesService {
   }
 
   public async getData(termBeginning: string, actualRank: TaxonRank, superiorTaxonRanks: TaxonRank[]) : Promise<TaxonData[]> {
-    return await fetch(this.apiUrl + "/taxa/autocomplete?taxonomicRanks=" + actualRank.rank +  "&term=" + termBeginning + "&page=1&size=10")
+    let smallestFilterRank: TaxonRank | undefined = superiorTaxonRanks.find(rank => rank.id != -1);
+
+    if (smallestFilterRank == undefined) {
+      return await fetch(this.apiUrl + "/taxa/autocomplete?taxonomicRanks=" + actualRank.rank +  "&term=" + termBeginning + "&page=1&size=10")
             .then(res => res.ok ? res.json() : {_embedded: null})
             .then(res => {
               return (res._embedded ?? {taxa: []}).taxa.map((element: { id: number; scientificName: string; }) => 
                 new TaxonData(element.id, element.scientificName)
               );
             });
+    }
+    let page: number = 1;
+    let result: TaxonData[] = [];
+    while (result.length != 10) {
+        let taxonData: TaxonData | null = await fetch(this.apiUrl + "/taxa/autocomplete?taxonomicRanks=" + actualRank.rank +  "&term=" + termBeginning + "&page=" + page + "&size=1")
+            .then(res => res.ok ? res.json() : {_embedded: null})
+            .then(async res => {
+              if (res._embedded == null) {
+                return null;
+              }
+              let taxa = res._embedded.taxa[0];
+              let id = taxa.id;
+              let name = taxa.name;
+              if (await this.isTaxonSonOfSelectedTaxonId(id, smallestFilterRank.id)) {
+                return new TaxonData(id, name);
+              } else {
+                return new TaxonData(-1, 'invalid');
+              }
+            });
+        if (taxonData == null) {
+          break;
+        }
+        if (taxonData.id != -1) {
+          result.push(taxonData);
+        }
+      }
+      return result;
+  }
+
+  private async isTaxonSonOfSelectedTaxonId(taxonId: number, parentId: number): Promise<boolean> {
+    return fetch(this.apiUrl + "/taxa/" + taxonId + "/classification")
+      .then(res => res.ok ? res.json() : { _embedded: { taxa: [] } })
+      .then(res => {
+        let parents = res._embedded.taxa;
+        return parents.some((parent: { id: number; }) => parent.id == parentId);
+      });
   }
 }
 
